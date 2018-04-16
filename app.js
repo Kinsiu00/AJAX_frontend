73a5b564addfcf0d04057337f92acb16c1d071c3
Kin Siu <kina.siu@gmail.com>
2018-04-16 10:07:53 -0400

    nooo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => {
+  let currentID;
   console.log("load is running!")
   const baseURL = 'http://localhost:3000/blogposts/';
   const pullPosts = () => {
@@ -6,14 +7,124 @@ window.addEventListener('load', () => {
     axios.get(`${baseURL}`)
       .then(response => {
         const list = response.data;
-        console.log(list);
 
+//CREATE
+        let newPostButton = document.createElement('button');
+        newPostButton.innerHTML = "New Post";
+        let buttons = document.getElementById('buted');
+        buttons.appendChild(newPostButton);
+
+        newPostButton.addEventListener('click', event => {
+          while (buttons.hasChildNodes()) {
+            buttons.removeChild(buttons.firstChild);
+          }
+          const forms = document.querySelector('form')
+          const buttons = document.getElementById('buted')
+
+          //make submit and cancel buttons
+          const submitButton = document.createElement('button')
+          submitButton.innerHTML = "submit";
+          const cancelButton = document.createElement('button')
+          cancelButton.innerHTML = "cancel";
+          buttons.appendChild(submitButton)
+          buttons.appendChild(cancelButton)
+
+          let formT = document.createElement('input');
+          formT.type = "text"
+          formT.name = "title"
+          formT.value = "test"
+          forms.insertAdjacentElement('afterbegin', formT)
+
+          let formC = document.createElement('input');
+          let spacer = document.createElement('br');
+
+          formC.type = "text"
+          formC.name = "content"
+          formC.value = "test"
+          forms.insertAdjacentElement('afterbegin', spacer)
+          forms.insertAdjacentElement('afterbegin', formC)
+
+          event.preventDefault();
+        })
+
+//SELECT POST
         list.forEach( post => {
           const li = document.createElement('li');
           li.innerHTML = `${post.title}`;
           blogPostList.appendChild(li);
-          const navList = document.getElementById('blogPostList');
+          const navList = document.getElementById('allPosts');
           navList.appendChild(li);
+
+          li.addEventListener('click', list => {
+            currentID = post.id
+            const mainT = document.getElementById('mainTitle')
+            mainT.innerHTML = `${post.title}`;
+            const mainC = document.getElementById('mainContent')
+            mainC.innerHTML = `${post.content}`;
+
+            //ensure buttons do not duplicate
+            const forms = document.querySelector('form')
+            const buttons = document.getElementById('buted')
+            while (buttons.hasChildNodes()) {
+              buttons.removeChild(buttons.firstChild);
+            }
+            //make edit and delete buttons
+            const editButton = document.createElement('button')
+            editButton.innerHTML = "edit";
+            const deleteButton = document.createElement('button')
+            deleteButton.innerHTML = "delete";
+            buttons.appendChild(editButton)
+            buttons.appendChild(deleteButton)
+
+            //when clicking the edit button
+            editButton.addEventListener('click', event =>{
+              while (buttons.hasChildNodes()) {
+                buttons.removeChild(buttons.firstChild);
+              }
+
+              //make submit and cancel buttons
+              const submitButton = document.createElement('button')
+              submitButton.innerHTML = "submit";
+              const cancelButton = document.createElement('button')
+              cancelButton.innerHTML = "cancel";
+              buttons.appendChild(submitButton)
+              buttons.appendChild(cancelButton)
+
+            mainT.innerHTML = "";
+            mainC.innerHTML = "";
+
+            //create forms and populating
+            let formT = document.createElement('input');
+            formT.type = "text"
+            formT.name = "title"
+            formT.value = `${post.title}`
+            forms.insertAdjacentElement('afterbegin', formT)
+
+            let formC = document.createElement('input');
+            let spacer = document.createElement('br');
+
+            formC.type = "text"
+            formC.name = "content"
+            formC.value = `${post.content}`
+            forms.insertAdjacentElement('afterbegin', spacer)
+            forms.insertAdjacentElement('afterbegin', formC)
+
+
+            //SUBMISSION
+            submitButton.addEventListener('click', event => {
+              console.log('submit button clicked')
+              event.preventDefault();
+            })
+
+              event.preventDefault();
+            })
+
+            //when clicking the delete button
+            deleteButton.addEventListener('click', event => {
+              console.log(`${post.title} delete`)
+              event.preventDefault();
+            })
+          })
         })
 
       })
